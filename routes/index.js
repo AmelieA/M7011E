9e414e769639ecee3139a60870ba8b31789daea6
AmelieA <amlaga-3@student.ltu.se>
2013-11-18 15:12:15 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'VISA' });
+};
+
+
+exports.mapbox = function(req, res){
+  res.sendfile('views/mapbox.html');
+};
