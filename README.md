bf2db86a80333c712b4b10966a9017b97e0925ae
AmelieA <amlaga-3@student.ltu.se>
2013-11-21 17:15:24 +0100

    little improvements

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Project for the course M7011E: Design of Dynamic Web Systems at LTU
 
-[b]Installing everything[/b]
+##Installing everything
 
 to install node.js : http://nodejs.org/.
 to download everything with git : sudo git clone git://github.com/AmelieA/M7011E.git.
 to install dependencies : npm install.
 to launch the server : node app.
 to go to the map locally : http://localhost:8080/mapbox
+to see the demo : http://54.194.9.139:8080/mapbox
--- app.js
+++ app.js
@@ -9,8 +9,10 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var html=require('html');
+var net = require('net');
 
 var app = express();
+var server = http.createServer(app);
 
 // all environments
 app.set('port', process.env.PORT || 8080);
@@ -41,6 +43,14 @@ app.use(function(req, res, next){
   res.send(404, 'Sorry cant find that!');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+  address = server.address();
+  console.log("Express server listening on %j", address);
+});
+
+var client = net.connect({port: app.get('port')},function() { //'connect' listener
+  server.getConnections(function(err, count){
+	console.log('There is '+count+' user connection');
+	});
 });
