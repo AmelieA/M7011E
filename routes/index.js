525beaccaa92fd8628341d24eb8bfaf8e60791bd
Polo417 <Polo417@users.noreply.github.com>
2013-11-27 19:04:46 +0100

    Update index.js
    Same request bug fixed.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var url = require('url');
 var querystring = require('querystring');
-
 var pg = require('pg').native;
 var dbURL = "tcp://nodetest:pika@localhost/dbtest";
 
@@ -14,25 +13,49 @@ exports.index = function(req, res){
 };
 
 
-exports.mapbox = function(req, res){
-  res.sendfile('views/mapbox.html');
-};
-
-exports.addpin = function(req, res)
+exports.mapbox = function(req, res)
 {
+	var user = req.params.user;
 	var params = querystring.parse(url.parse(req.url).query)
+	var already_exists = 0;
+	
+	
 	
 	if ('name' in params && 'x' in params && 'y' in params && 'comment' in params && 'login' in params) 
-	{
-		pg.connect(dbURL, function(err, client) {
-			client.query("INSERT INTO Locations(location, x, y, img_name) VALUES($1, $2, $3, $4)",[params['name'], params['x'], params['y'], "null"]);
-			client.query("INSERT INTO Comments(location, text, login) VALUES($1, $2, $3)",[params['name'], params['comment'], "default_user"]);
-			console.log("Added : name = " + params['name'] + ", x = " + params['x'] + ", y = " + params['y'] + ", comment = " + params['comment'] + ", login = " + params['login']);
-		})
+	{	
+		pg.connect(dbURL, 	function(err, client)
+							{      
+								client.query("SELECT * FROM Locations", 					function(err, result) 
+																		{
+																			for (var i = 0; i < result.rows.length; i++) 
+																			{
+																				var row = result.rows[i];
+																				if (row.location==params['name']) 
+																				{
+																					already_exists=1;
+																				}
+
+																			}
+																			
+																			if (already_exists==0)
+																			{											
+																				client.query("INSERT INTO Locations(location, x, y, img_name) VALUES($1, $2, $3, $4)",[params['name'], params['x'], params['y'], "null"]);
+																				client.query("INSERT INTO Comments(location, text, login) VALUES($1, $2, $3)",[params['name'], params['comment'], "default_user"]);
+																				console.log("Added : name = " + params['name'] + ", x = " + params['x'] + ", y = " + params['y'] + ", comment = " + params['comment'] + ", login = " + params['login']);
+
+
+																			}
+																			else
+																			{
+																				console.log("Can't add location : it already exists.");
+																			}
+																		})
+							});
     }
-    else {
+    else 
+    {
         console.log('Error : invalid request');
     }
-    
-  res.sendfile('views/mapbox.html');
+	res.sendfile('views/mapbox.html'); //{user: 'user'}
+
 };
