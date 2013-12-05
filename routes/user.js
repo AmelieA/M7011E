bce055b86fc579c45b9bb32de12cec4d6968e8ab
AmelieA <amlaga-3@student.ltu.se>
2013-12-05 10:38:14 +0100

    almost donne with the user gestion

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,38 @@ var app = require('../app');
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  res.sendfile('views/Users.html');
+  app.io.sockets.on('connection', function (socket) {
+		pg.connect(dbURL, 	function(err, client) {      
+			client.query("SELECT * FROM Users", function(err, result) {
+				console.log(result);
+				socket.emit('displayUsers', result);
+				});
+			});
+		socket.on('AskForBan',function (data) {
+			console.log("ask for ban");
+			pg.connect(dbURL, 	function(err, client) {
+				for (var i=0; i<data.banIDs.length; i++){
+					console.log(data.banIDs[i]);
+					client.query("UPDATE Users SET banned = 'true' WHERE googleid = '"+data.banIDs[i]+"';", function(err, result) {
+						console.log("ban of "+data.banIDs[i]);
+					});
+				}
+			});
+		});
+		socket.on('AskForUnBan',function (data) {
+			console.log("ask for unban");
+			pg.connect(dbURL, 	function(err, client) {
+				for (var i=0; i<data.banIDs.length; i++){
+					console.log(data.banIDs[i]);
+					client.query("UPDATE Users SET banned = 'false' WHERE googleid = '"+data.banIDs[i]+"';", function(err, result) {
+						console.log("unban of "+data.banIDs[i]);
+					});
+				}
+			});
+		});
+	});
+	//~ res.redirect("/users");
 };
 
 exports.checkUser = function(FirstName, LastName, GoogleID){
@@ -19,8 +50,8 @@ exports.checkUser = function(FirstName, LastName, GoogleID){
 	pg.connect(dbURL, function(err, client){      
 		client.query("SELECT * FROM Users", function(err, result) {
 			for (var i = 0; i < result.rows.length; i++) {
-				console.log(result.rows[i].googleid,GoogleID)
-				if (result.rows[i].googleid == GoogleID){
+				console.log(parseInt(result.rows[i].googleid), parseInt(GoogleID))
+				if (parseInt(result.rows[i].googleid) == parseInt(GoogleID)){
 					exist = true;
 					ban = result.rows[i].banned
 				}
@@ -32,6 +63,8 @@ exports.checkUser = function(FirstName, LastName, GoogleID){
 			client.query("INSERT INTO Users(firstName, lastName, googleID, banned) VALUES('"+FirstName+"', '"+LastName+"', '"+GoogleID+"', false)");
 			console.log("adding "+FirstName+" into the database");
 		
+		}else{
+			console.log(FirstName+" is already in the database");
 		}
 	})
 	return(ban);
